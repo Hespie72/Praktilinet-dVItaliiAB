b8865b17649bb44d3e7161e3114562b5d19a8c90
opilane <opilane@HP-CZC2349HV0>
2026-03-05 13:14:11 +0200

    Add project files.

---
+++ StartPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+Console.OutputEncoding = Encoding.UTF8;
+
+namespace PraktilinetöödVitaliiAB
+{
+    public class StartPage
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Valik 1-5 \n 1.Luua faili Retseptid \n 2.Loe fail \n \n \n ");
+                int valik = int.Parse(Console.ReadLine());
+                if (valik == 1)
+                {
+                    osa4P.ItalToit(); break;
+                }
+                else if (valik == 2)
+                {
+                    osa4P.retseptid_loe(); break;
+                }
+            }
+        }
+    }
+}
---
+++ osa4P.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PraktilinetöödVitaliiAB
+{
+    internal class osa4P
+    {
+        public static void ItalToit()
+        {
+            try
+            {
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Retseptid.txt"); //@"..\..\..\Kuud.txt"
+                StreamWriter text = new StreamWriter(path, true); // true = lisa lõppu
+                Console.WriteLine("Sisesta min 2 itaalia toidu: ");
+                string lause = Console.ReadLine();
+                text.WriteLine(lause);
+                text.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Mingi viga failiga");
+            }
+        }
+        public static List<string> retseptid_loe()
+        {
+            List<string> toit_list = new List<string>();
+            try
+            {
+                string path = @$"..\..\..\Retseptid.txt";
+                foreach (string rida in File.ReadAllLines(path))
+                {
+                    toit_list.Add(rida);
+                }
+                foreach (string i in toit_list) Console.WriteLine(i);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Viga failiga");
+            }
+            return toit_list;
+        }
+
+    }
+}
